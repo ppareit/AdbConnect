8106bd86ad5ac9d2e3911310602d3e0153b66da0
Pieter Pareit <pieter.pareit@gmail.com>
2011-08-31 14:47:07 +0200

    Cleanup includes and comment

--- src/adbconnect/handlers/ConnectHandler.java
+++ src/adbconnect/handlers/ConnectHandler.java
@@ -22,19 +22,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.Inet6Address;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Enumeration;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -44,18 +32,16 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.menus.UIElement;
-import org.eclipse.jface.resource.ImageDescriptor;
 
 import adbconnect.Activator;
 
 
 /**
  * Handles the executing of the command, and the updating of the button.
- * 
- * TODO: ip address is currently hard-coded
  */
 public class ConnectHandler extends AbstractHandler implements IElementUpdater {
 
